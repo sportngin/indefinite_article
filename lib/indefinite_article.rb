8c9c8481ed3a9de8d20d9b542ac58ae020fb22aa
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2012-11-26 11:43:39 -0600

    added more exceptions, including the controversial historic to the list

--- lib/indefinite_article.rb
+++ lib/indefinite_article.rb
@@ -3,7 +3,7 @@ require 'active_support'
 require 'active_support/core_ext/string'
 
 module IndefiniteArticle
-  WORDS_WITH_INITIAL_VOWELS_THAT_ACT_LIKE_WORDS_WITH_INITIAL_CONSONANTS = %w(one united)
+  WORDS_WITH_INITIAL_VOWELS_THAT_ACT_LIKE_WORDS_WITH_INITIAL_CONSONANTS = %w(one united honorable honest hour historic union unicorn used U.S.)
   INDEFINITE_ARTICLES = { :vowel => 'an', :consonant => 'a'}
   VOWELS = %w(a e i o u)
 end
