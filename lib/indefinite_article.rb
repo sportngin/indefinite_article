b8d3bb32b9635dfdae322ce80297e630f32bf108
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2012-11-26 11:51:30 -0600

    added list of words with initial consonants that act like words with initial vowels

--- lib/indefinite_article.rb
+++ lib/indefinite_article.rb
@@ -3,7 +3,8 @@ require 'active_support'
 require 'active_support/core_ext/string'
 
 module IndefiniteArticle
-  WORDS_WITH_INITIAL_VOWELS_THAT_ACT_LIKE_WORDS_WITH_INITIAL_CONSONANTS = %w(one united honorable honest hour historic union unicorn used U.S.)
+  WORDS_WITH_INITIAL_VOWELS_THAT_ACT_LIKE_WORDS_WITH_INITIAL_CONSONANTS = %w(one united union unicorn used U.S.)
+  WORDS_WITH_INITIAL_CONSONANTS_THAT_ACT_LIKE_WORDS_WITH_INITIAL_VOWELS = %w(honorable honest hour historic)
   INDEFINITE_ARTICLES = { :vowel => 'an', :consonant => 'a'}
   VOWELS = %w(a e i o u)
 end
--- lib/indefinite_article/articulated.rb
+++ lib/indefinite_article/articulated.rb
@@ -3,6 +3,8 @@ module IndefiniteArticle
     def indefinite_article
       if ::IndefiniteArticle::WORDS_WITH_INITIAL_VOWELS_THAT_ACT_LIKE_WORDS_WITH_INITIAL_CONSONANTS.include? to_s._first_word_for_indefinite_article._first_term_for_indefinite_article.downcase
         ::IndefiniteArticle::INDEFINITE_ARTICLES[:consonant]
+      elsif ::IndefiniteArticle::WORDS_WITH_INITIAL_CONSONANTS_THAT_ACT_LIKE_WORDS_WITH_INITIAL_VOWELS.include? to_s._first_word_for_indefinite_article._first_term_for_indefinite_article.downcase
+        ::IndefiniteArticle::INDEFINITE_ARTICLES[:vowel]
       elsif VOWELS.include? self.to_s.first.downcase
         ::IndefiniteArticle::INDEFINITE_ARTICLES[:vowel]
       else
